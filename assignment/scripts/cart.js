ff07472b161bc7bbfcfb81e68a2fa31263cb43cf
Everett Butler <everettjamesbutler@gmail.com>
2023-01-25 20:27:28 -0600

    rough draft with group will clean up later!

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,49 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+const maxItems = 5;
+console.log (`basket is ${basket}`);
+
+function addItem( item ) {
+    console.log( isFull() );
+    if ( isFull() === true){
+        isFull( basket );
+        basket.push( item );
+        return true;
+    }
+}
+addItem( 'chips' );
+addItem( 'bread' );
+addItem( 'milk' );
+addItem( 'cookies' );
+addItem( 'popcorn' );
+addItem( 'cheese' );
+console.log ( basket );
+
+function listItems() {
+    for(let groceries of basket) {
+        console.log(groceries);
+    }
+}
+
+listItems();
+
+function empty() {
+   basket = [];
+}
+
+empty();
+console.log (basket);
+
+
+
+function isFull() {
+    if (basket.length < maxItems) {
+    return true;
+    } else {
+    return false;
+    }
+}
+
